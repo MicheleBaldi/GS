84db1683987999170bf1592b73de2c8e9ca89a6b
Michele Baldi <m.baldi@sintraconsulting.eu>
2023-06-17 14:57:47 +0200

    update fuction

--- netlify/functions/uscite.ts
+++ netlify/functions/uscite.ts
@@ -5,7 +5,7 @@ import Airtable from 'airtable';
 const { AIRTABLE_KEY } = process.env;
 
 // USE YOUR TABLE BASE HERE
-const base = new Airtable({ apiKey: 'patwyqLWjyZr5xPay.a9dc6101277f1b32e514d7f8c02310c59007a55a85508ce4e76f297a21e7f4ef' }).base('appeK7aRGtPSKdLMp');
+const base = new Airtable({ apiKey: AIRTABLE_KEY }).base('appeK7aRGtPSKdLMp');
 
 const handler: Handler = async (event: HandlerEvent, context: any) => {
 	try {
