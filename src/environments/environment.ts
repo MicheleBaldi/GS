34346a019bfdc2cc86d711ed0f99da9acccffe3a
Michele Baldi <m.baldi@sintraconsulting.eu>
2024-03-29 13:03:54 +0100

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: true,
     auth0: {
-      domain: 'dev-vs8dyay8d5sqq717.us.auth0.com',
-      clientId: 'cE0OM5S9HIwk5pYcEWDrNtZK2cTeyahB',
+      domain: 'gscortona.us.auth0.com',
+      clientId: 'WZzQptBk8JU5ymEtJZD5uFx6HGa6zujX',
       authorizationParams: {
         redirect_uri: window.location.origin,
       },
