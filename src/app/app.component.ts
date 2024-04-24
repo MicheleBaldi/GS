3cfedfbda6248754f7a56fd9682305bea188a11a
Michele Baldi <m.baldi@sintraconsulting.eu>
2024-04-24 15:03:49 +0200

    WIP notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { HttpClient } from '@angular/common/http';
 import { DataService } from './service/data.service';
 import { AuthService } from '@auth0/auth0-angular';
 import { Router } from '@angular/router';
+import { SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -19,11 +20,25 @@ export class AppComponent {
   user:any;
   imgProfile:string="/assets/gs.jpg";
 
+  readonly VAPID_PUBLIC_KEY = "BLBx-hf2WrL2qEa0qKb-aCJbcxEvyn62GDTyyP9KTS5K7ZL0K7TfmOKSPqp8vQF0DaG8hpSBknz_x3qf5F4iEFo";
+
+    
+
   constructor(private observer: BreakpointObserver,
     private http: HttpClient,
     public dataService: DataService,
     public auth: AuthService,
-    private router:Router,) {}
+    private router:Router,
+    private swPush: SwPush) {}
+
+    subscribeToNotifications() {
+
+      this.swPush.requestSubscription({
+          serverPublicKey: this.VAPID_PUBLIC_KEY
+      })
+      .then(sub => console.log(sub))
+      .catch(err => console.error("Could not subscribe to notifications", err));
+  }
 
   ngOnInit(){
     this.auth.isAuthenticated$.subscribe({
